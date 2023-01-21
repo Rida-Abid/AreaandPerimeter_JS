05b7362f442ce7f95885b3f54d9bf69b5435574d
Rida-Abid <107047216+Rida-Abid@users.noreply.github.com>
2023-01-21 22:17:41 +0500

    Wrote the program that calculates area and perimeter of square rectangle and triangle, given length bredth and height and 's', 'r,'t' as input for shape

--- AreaandPerimeter/wwwroot/js/Program.js
+++ AreaandPerimeter/wwwroot/js/Program.js
@@ -1,4 +1,83 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿var arr = [];
+arr = prompt("Enter your numbers").split(" ");
+Main(arr);  
 
-// Write your JavaScript code.
+function Main(arr) {
+
+   
+
+    
+    
+        var shape = arr[0];
+        var length = parseFloat(arr[1]);
+        var bredth = parseFloat(arr[2]);
+        var height = parseFloat(arr[3]);
+
+       
+
+        
+
+        
+
+            function areaofSquare(length) {
+                return length * length;
+            }
+
+            function perimeterofSquare(length) {
+                return 4 * length;
+            }
+
+            function areaofTriangle(length, bredth) {
+                return (length * bredth) / 2;
+            }
+
+            function perimeterofTriangle(length, bredth, height) {
+                return length + bredth + height;
+            }
+
+            function areaofRectangle(length, bredth) {
+                return length * bredth;
+            }
+
+            function perimeterofRectangle(length, bredth) {
+                return 2 * (length * bredth);
+            }
+
+
+
+
+
+
+            function DisplayValues(heading, result) {
+                console.log("-----------------------------");
+                console.log(heading);
+
+                console.log(result);
+
+            }
+
+            if (shape.toLowerCase() == "s") {
+                DisplayValues("Area of Square is:", areaofSquare(length));
+                DisplayValues("Perimeter of Square is:", perimeterofSquare(length));
+            }
+
+            else if (shape.toLowerCase() == "t") {
+                DisplayValues("Area of Triangle is:", areaofTriangle(length, bredth));
+                DisplayValues("Perimeter of Triangle is:", perimeterofTriangle(length, bredth, height));
+            }
+
+
+            else if (shape.toLowerCase() == "r") {
+                DisplayValues("Area of Rectangle is:", areaofRectangle(length, bredth));
+                DisplayValues("Perimeter of Rectangle is:", perimeterofRectangle(length, bredth));
+            }
+
+            else {
+                console.log("Invalid Choice for Shape, Enter S,R or T for Square, Rectangle or Triangle");
+            }
+
+
+        
+
+    
+}
